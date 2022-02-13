61677906e267662bb6d9d9e8e7a5aae0d7360f2b
Serhii Marchenko <serge5709244@gmail.com>
2022-02-12 20:40:27 -0500

    Fixed diagonal movement being faster

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,10 +17,21 @@ public class Player : MonoBehaviour
 
     void CheckInput()
     {
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
-        transform.position += new Vector3(x, y, 0);
+        float x = Input.GetAxis("Horizontal"); //  Get input
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(x, y, 0);   //  Get direction vector
+
+        if(moveDirection.magnitude > 1) //  Limit this vector magnitude to 1
+        {
+            moveDirection = moveDirection / moveDirection.magnitude;
+        }
+
+        //Debug.Log(moveDirection);
+        //Debug.Log(moveDirection.magnitude);
+
+        transform.position += moveDirection * speed * Time.deltaTime;    //  Apply movement
 
     }
 }
