c8e8dee7bb6553477f41dfa117df8135f99819a9
AhmetPolat06 <savosaren06@gmail.com>
2022-03-15 09:56:38 -0400

    Player respawn in the Hub after died

---
+++ Assets/Scripts/ReturnHub.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnHub : MonoBehaviour
+{
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
