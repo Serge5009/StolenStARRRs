59c01b00b6e1069fe8877c59b6235c66a2c019d0
Serhii Marchenko <serge5709244@gmail.com>
2022-03-27 17:38:47 -0400

    Added enemy animation in code

--- Assets/Scripts/AIMovement.cs
+++ Assets/Scripts/AIMovement.cs
@@ -9,17 +9,37 @@ public class AIMovement : MonoBehaviour
 
     public float speed = 0.0f; // tokyo drift?
     private Transform target;
-    // Start is called before the first frame update
+
+    Animator animator;
+
+    void Awake()
+    {
+        GameObject sprite = gameObject.transform.GetChild(0).gameObject;
+        animator = sprite.GetComponent<Animator>();
+        if (!animator)
+        {
+            Debug.Log("No animator attached to enemy");
+        }
+    }
+
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); // here we are getting players location
         speed += Random.Range(speed / -5, speed / 5);  //  Randomizing speed
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Vector2.Distance(transform.position, target.position) > stopDistance)  // in order to avaoid collapsing with player
+        {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime); // imma chase the poor player guy endlessly
+            animator.SetBool("IsRunning", true);
+
+        }
+        else
+        {
+            animator.SetBool("IsRunning", false);
+        }
+
     }
 }
