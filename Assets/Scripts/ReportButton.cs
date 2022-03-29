e3729ee6be4681a539158d889691d17f2579304c
Sam <sam.d.switzer@gmail.com>
2022-03-28 21:44:51 -0400

    Added report button script

---
+++ Assets/Scripts/ReportButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReportButton : MonoBehaviour
+{
+    public void OpenChannel()
+    {
+        Application.OpenURL("https://forms.gle/V68kgKGgivm1iJkQA");
+    }
+}
